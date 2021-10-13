36d5f9fee03af290b338a45e1a1d378f3d375577
www111c1 <2565398391@qq.com>
2021-10-13 19:27:38 +0800

    Initial commit

---
+++ src/main/java/com/ssm/WysssApplication.java
@@ -0,0 +1,13 @@
+package com.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WysssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WysssApplication.class, args);
+    }
+
+}
