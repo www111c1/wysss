2bedcb73a80f726831597fbc1977dc6dea9f9d11
www111c1 <2565398391@qq.com>
2021-10-13 19:32:45 +0800

    jkl01

--- src/main/java/com/ssm/WysssApplication.java
+++ src/main/java/com/ssm/WysssApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WysssApplication {
-//yuii
+//yuiio
     public static void main(String[] args) {
         SpringApplication.run(WysssApplication.class, args);
     }
